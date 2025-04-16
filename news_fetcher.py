8831f9c56ca0ed3e8d6f6d22687fc07a87d345a5
wannabeaquant <atharvasingh0405@gmail.com>
2025-04-16 15:07:58 +0530

    Final Changes

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,6 +1,6 @@
 import feedparser
 
-def get_google_news_rss(ticker, limit=5):
+def get_google_news_rss(ticker, limit=20):
     query = ticker.replace(" ", "+")
     feed_url = f"https://news.google.com/rss/search?q={query}+stock&hl=en-US&gl=US&ceid=US:en"
     feed = feedparser.parse(feed_url)
